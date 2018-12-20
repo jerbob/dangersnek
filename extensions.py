270470ab7710a241c9fb72b8f19ba74659b2a140
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-12-20 14:09:42 +0000

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,20 +1,8 @@
-from mitmproxy.http import HTTPFlow
+"""Constants for use in the main application."""
+
+from os import environ
 
 
-def _add_stop(flow: HTTPFlow, latitude: float, longitude: float) -> HTTPFlow:
-    """Add a stop to the given Google Maps request."""
-    url = flow.request.url
-    if not url.startswith('https://www.google.com/maps/dir'):
-        return
-    print(f'[?] Request intercepted: {url}')
-    url = url.split('/')
-    for i, section in enumerate(url):
-        if section.startswith('@'):
-            break
-    url.insert(i, f"'{latitude},{longitude}'")
-    print(
-        f'[+] Injected detour with latitude {latitude}, '
-        f'longitude {longitude}'
-    )
-    flow.request.url = '/'.join(url)
-    print(f'[?] New request string: {flow.request.url}')
+API_PARAMS = {
+    'key': environ.get('TOMTOM_API_KEY')
+}
