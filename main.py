2f38c20ee818f49e6185a123a42c0c1f11bd3e65
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-12-20 14:08:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from datetime import datetime
-
-import requests
-from typing import Collection
-
-from extensions import _add_stop
+from typing import collection
 
 from mitmproxy.http import HTTPFlow
+import requests
 
 
 endpoint = 'https://api.tomtom.com/search/2/geocode/{}.json'
 
-parameters = {
-    'key': 'xbOQYdu7BJG9tEZhFrPnPQq4En9FHv6D'
-}
 
+def _add_stop(flow: HTTPFlow, latitude: float, longitude: float) -> HTTPFlow:
+    """Add a stop to the given Google Maps request."""
+    url = flow.request.url
+    if not url.startswith('https://www.google.com/maps/dir'):
+        return
+    print(f'[?] Request intercepted: {url}')
+    url = url.split('/')
+    for i, section in enumerate(url):
+        if section.startswith('@'):
+            break
+    url.insert(i, f"'{latitude},{longitude}'")
+    print(
+        f'[+] Injected detour with latitude {latitude}, '
+        f'longitude {longitude}'
+    )
+    flow.request.url = '/'.join(url)
+    print(f'[?] New request string: {flow.request.url}')
 
-def request(flow: HTTPFlow):
-    _add_stop(flow, 53.4497072, -2.261636)
 
 def _geocode_address(address: str) -> Collection[float]:
+    """Given an address, return the latitude and longitude values."""
     print(f'[?] Querying address: {endpoint.format(address)}')
     result = requests.get(
         endpoint.format(address), params=parameters
@@ -28,6 +38,7 @@ def _geocode_address(address: str) -> Collection[float]:
 
 
 def _nearest_crime(longitude: float, latitude: float) -> str:
+    """Find the nearest crime to a given latitude/longitude point."""
     print(f'[?] Querying crimes: {longitude, latitude}')
     current_date = datetime.now().strftime('%Y-%m')
     print(current_date)
@@ -39,5 +50,5 @@ def _nearest_crime(longitude: float, latitude: float) -> str:
 
 
 def _crime_from_address(address: str) -> str:
+    """Find the nearest crime to a given address."""
     return _nearest_crime(*_geocode_address(address))
-
